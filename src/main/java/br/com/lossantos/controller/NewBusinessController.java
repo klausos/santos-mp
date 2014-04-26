c54846cbe6c404d0164ddacb2a93372ebb11b00e
Klaus <klaus@ganondorf.localhost>
2014-04-26 15:01:00 -0300

    Image upload processing.

--- src/main/java/br/com/lossantos/controller/NewBusinessController.java
+++ src/main/java/br/com/lossantos/controller/NewBusinessController.java
@@ -1,13 +1,23 @@
 package br.com.lossantos.controller;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 import javax.validation.Valid;
+
+import net.sf.jmimemagic.Magic;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -28,6 +38,9 @@ public class NewBusinessController {
 	@Autowired
 	private BusinessDao businessDao;
 
+	@Value("${upload.path.businesses}")
+	private String uploadPath;
+
 	@RequestMapping(value = "/cadastro.html", method = RequestMethod.GET)
 	public String home(Business business, Model model) {
 		model.addAttribute("business", business);
@@ -40,15 +53,58 @@ public class NewBusinessController {
 			BindingResult result, Model model,
 			@RequestParam("image") MultipartFile uploadedImage) {
 
+		validateUploadedImage(uploadedImage, result);
+
 		model.addAttribute("business", business);
 		model.addAttribute("validationResult", result);
 
 		if (result.hasErrors() == false) {
 			logger.info("About to persist business object: {}", business);
 			businessDao.add(business);
+
+			handleImageUpload(business.getId(), uploadedImage);
 		}
 
 		return "new-business";
+	}
+
+	// TODO add resizing code here: http://code.google.com/p/java-image-scaling/
+	private void validateUploadedImage(MultipartFile uploadedImage,
+			BindingResult result) {
+		if (uploadedImage.getSize() == 0) {
+			result.addError(new FieldError("business", "image", "invalid-image"));
+		} else {
+			try {
+				String mimeType = Magic.getMagicMatch(uploadedImage.getBytes(),
+						false).getMimeType();
+				if (!mimeType.startsWith("image")) {
+					result.addError(new FieldError("business", "image",
+							"invalid-image"));
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private void handleImageUpload(Long businessId, MultipartFile uploadedImage) {
+		// handling image upload
+		File uploadDir = new File(uploadPath + File.separator + businessId);
+		uploadDir.mkdirs();
+
+		File targetFile = new File(uploadDir.getAbsolutePath() + File.separator
+				+ uploadedImage.getOriginalFilename());
+
+		try {
+			BufferedOutputStream stream = new BufferedOutputStream(
+					new FileOutputStream(targetFile));
+			stream.write(uploadedImage.getBytes());
+			stream.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@InitBinder
